b3895917e06c0ce5dbbe70d9c3b9d913c9f5ddae
Dan Balasescu <smoogipoo@smgi.me>
2022-05-11 14:06:11 +0900

    Rework PP gen to use full-outer joins
    As per previous commit.

--- run-pp.sh
+++ run-pp.sh
@@ -25,94 +25,66 @@ case "$MODE" in
         ;;
 esac
 
-# Get combined PP gains
-mysql -uroot --execute="
-    SELECT
-        m.score_id,
-        m.beatmap_id,
-        m.enabled_mods,
-        b.filename,
-        m.pp as 'pp_master',
-        p.pp as 'pp_pr',
-        (p.pp - m.pp) as 'diff',
-        (p.pp / m.pp - 1) as 'diff%'
-    FROM osu.osu_scores${HIGH_SCORES_SUFFIX}_high m
-    JOIN osu_pr.osu_scores${HIGH_SCORES_SUFFIX}_high p
-        ON m.score_id = p.score_id
-    JOIN osu_pr.osu_beatmaps b
-        ON b.beatmap_id = p.beatmap_id
-    WHERE abs(m.pp - p.pp) > 0.1
-    ORDER BY p.pp - m.pp
-        DESC
-    LIMIT 10000;" \
-> pp_gains_all.csv &
+function generate() {
+    mod_comparator=$1
+    sort_mode=$2
 
-# Get combined PP losses
-mysql -uroot --execute="
-    SELECT
-        m.score_id,
-        m.beatmap_id,
-        m.enabled_mods,
-        b.filename,
-        m.pp as 'pp_master',
-        p.pp as 'pp_pr',
-        (p.pp - m.pp) as 'diff',
-        (p.pp / m.pp - 1) as 'diff%'
-    FROM osu.osu_scores${HIGH_SCORES_SUFFIX}_high m
-    JOIN osu_pr.osu_scores${HIGH_SCORES_SUFFIX}_high p
-        ON m.score_id = p.score_id
-    JOIN osu_pr.osu_beatmaps b
-        ON b.beatmap_id = p.beatmap_id
-    WHERE abs(m.pp - p.pp) > 0.1
-    ORDER BY p.pp - m.pp
-        ASC
-    LIMIT 10000;" \
-> pp_losses_all.csv &
+    mysql -uroot --execute="
+        SELECT
+            tbl.score_id,
+            b.beatmap_id,
+            tbl.enabled_mods,
+            b.filename,
+            tbl.a_pp as 'pp_master',
+            tbl.b_pp as 'pp_pr',
+            (tbl.b_pp - tbl.a_pp) as 'diff',
+            (tbl.b_pp / tbl.a_pp - 1) as 'diff%'
+        FROM (
+            SELECT
+                a.score_id,
+                a.beatmap_id,
+                a.enabled_mods,
+                a.pp AS 'a_pp',
+                b.pp AS 'b_pp'
+            FROM osu.osu_scores${HIGH_SCORES_SUFFIX}_high a
+            LEFT JOIN osu_pr.osu_scores${HIGH_SCORES_SUFFIX}_high b
+                ON a.score_id = b.score_id
+            # Simulate a full-outer join
+            UNION ALL
+                SELECT
+                    b.score_id,
+                    b.beatmap_id,
+                    b.enabled_mods,
+                    a.pp AS 'a_pp',
+                    b.pp AS 'b_pp'
+                FROM osu.osu_scores${HIGH_SCORES_SUFFIX}_high a
+                RIGHT JOIN osu_pr.osu_scores${HIGH_SCORES_SUFFIX}_high b
+                    ON a.score_id = b.score_id
+                WHERE a.score_id IS NULL # Anti-join
+        )
+        AS tbl
+        JOIN osu_pr.osu_beatmaps b
+            ON b.beatmap_id = tbl.beatmap_id
+        WHERE tbl.${mod_comparator}
+        AND (
+            tbl.a_pp IS NULL
+            OR tbl.b_pp IS NULL
+            OR abs(tbl.a_pp - tbl.b_pp) > 0.1
+        )
+        ORDER BY
+            # Nulls at the top of the list (most importance)
+            (
+                tbl.a_pp IS NULL
+                OR tbl.b_pp IS NULL
+            ) DESC,
+            # Then order by diff
+            tbl.b_pp - tbl.a_pp ${sort_mode}
+        LIMIT 10000;"
+}
 
-# Get NoMod PP gains
-mysql -uroot --execute="
-    SELECT
-        m.score_id,
-        m.beatmap_id,
-        m.enabled_mods,
-        b.filename,
-        m.pp as 'pp_master',
-        p.pp as 'pp_pr',
-        (p.pp - m.pp) as 'diff',
-        (p.pp / m.pp - 1) as 'diff%'
-    FROM osu.osu_scores${HIGH_SCORES_SUFFIX}_high m
-    JOIN osu_pr.osu_scores${HIGH_SCORES_SUFFIX}_high p
-        ON m.score_id = p.score_id
-    JOIN osu_pr.osu_beatmaps b
-        ON b.beatmap_id = p.beatmap_id
-    WHERE m.enabled_mods = 0
-    AND abs(m.pp - p.pp) > 0.1
-    ORDER BY p.pp - m.pp
-        DESC
-    LIMIT 10000;" \
-> pp_gains_nm.csv &
-
-# Get NoMod PP losses
-mysql -uroot --execute="
-    SELECT
-        m.score_id,
-        m.beatmap_id,
-        m.enabled_mods,
-        b.filename,
-        m.pp as 'pp_master',
-        p.pp as 'pp_pr',
-        (p.pp - m.pp) as 'diff',
-        (p.pp / m.pp - 1) as 'diff%'
-    FROM osu.osu_scores${HIGH_SCORES_SUFFIX}_high m
-    JOIN osu_pr.osu_scores${HIGH_SCORES_SUFFIX}_high p
-        ON m.score_id = p.score_id
-    JOIN osu_pr.osu_beatmaps b
-        ON b.beatmap_id = p.beatmap_id
-    WHERE m.enabled_mods = 0
-    AND abs(m.pp - p.pp) > 0.1
-    ORDER BY p.pp - m.pp
-        ASC
-    LIMIT 10000;" \
-> pp_losses_nm.csv &
+generate "enabled_mods >= 0" "DESC" > pp_gains_all.csv &
+generate "enabled_mods >= 0" "ASC" > pp_losses_all.csv &
+generate "enabled_mods = 0" "DESC" > pp_gains_nm.csv &
+generate "enabled_mods = 0" "ASC" > pp_losses_sr.csv &
 
 wait
